056d3d385b2448a63a67a209f7d0697f3e7fd4e0
salihct <salihct007@gmail.com>
2020-09-12 15:42:19 +0530

    beep sound fuction is crerted

---
+++ beep.py
@@ -0,0 +1,26 @@
+import pyaudio
+import numpy as np
+
+p = pyaudio.PyAudio()
+
+volume = 0.5     # range [0.0, 1.0]
+fs = 44100       # sampling rate, Hz, must be integer
+duration = 1.0   # in seconds, may be float
+f = 440.0        # sine frequency, Hz, may be float
+
+# generate samples, note conversion to float32 array
+samples = (np.sin(2*np.pi*np.arange(fs*duration)*f/fs)).astype(np.float32)
+
+# for paFloat32 sample values must be in range [-1.0, 1.0]
+stream = p.open(format=pyaudio.paFloat32,
+                channels=1,
+                rate=fs,
+                output=True)
+
+# play. May repeat with different volume values (if done interactively) 
+stream.write(volume*samples)
+
+stream.stop_stream()
+stream.close()
+
+p.terminate()
